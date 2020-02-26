36ad470b36d7465cec9c9d1f1b05a4b89df31bb2
Student <example@example.com>
2020-02-26 14:07:20 -0800

    Implement full reducer with tests

--- src/__tests__/tic-tac-toe-reducer.test.js
+++ src/__tests__/tic-tac-toe-reducer.test.js
@@ -14,23 +14,57 @@ describe('ticTacToeReducer', () => {
         };
         const action = { type: 'MAKE_MOVE', location: [0, 0] };
 
-        test('should assign player value to specified board location', () => {
-            expect(ticTacToeReducer(dummyState, action).board).toEqual([
-                [1, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0],
-            ]);
+        test.each([
+            [[0, 0], [[1, 0, 0], [0, 0, 0], [0, 0, 0]]],
+            [[0, 1], [[0, 1, 0], [0, 0, 0], [0, 0, 0]]],
+            [[0, 2], [[0, 0, 1], [0, 0, 0], [0, 0, 0]]],
+            [[1, 0], [[0, 0, 0], [1, 0, 0], [0, 0, 0]]],
+            [[1, 1], [[0, 0, 0], [0, 1, 0], [0, 0, 0]]],
+            [[1, 2], [[0, 0, 0], [0, 0, 1], [0, 0, 0]]],
+            [[2, 0], [[0, 0, 0], [0, 0, 0], [1, 0, 0]]],
+            [[2, 1], [[0, 0, 0], [0, 0, 0], [0, 1, 0]]],
+            [[2, 2], [[0, 0, 0], [0, 0, 0], [0, 0, 1]]],
+        ])('assigns player value to location - (%s)', (input, expected) => {
+            const result = ticTacToeReducer(dummyState, {
+                type: 'MAKE_MOVE',
+                location: input,
+            });
+            expect(result.board).toEqual(expected);
         });
-        test('should change activePlayer when a move is made', () => {
-            expect(ticTacToeReducer(dummyState, action).activePlayer).toEqual(
-                -1
-            );
-        });
-        test('should increment turnNumber when a move has been made', () => {
-            expect(ticTacToeReducer(dummyState, action).turnNumber).toEqual(2);
-        });
-        test('should increment turnNumber when a move has been made', () => {
-            expect(ticTacToeReducer(dummyState, action).turnNumber).toEqual(2);
-        });
+        test.each([[1, -1], [-1, 1]])(
+            'changes activePlayer - (%d => %d)',
+            (input, expected) => {
+                const result = ticTacToeReducer(
+                    { ...dummyState, activePlayer: input },
+                    action
+                );
+                expect(result.activePlayer).toEqual(expected);
+            }
+        );
+        test.each([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8]])(
+            'increment turnNumber - (%d => %d)',
+            (input, expected) => {
+                const result = ticTacToeReducer(
+                    { ...dummyState, turnNumber: input },
+                    action
+                );
+                expect(result.turnNumber).toEqual(expected);
+            }
+        );
+        test.each([
+            [[[0, 1, 1], [0, 0, 0], [0, 0, 0]], 'WIN'],
+            [[[0, 0, 0], [1, 0, 0], [1, 0, 0]], 'WIN'],
+            [[[0, 0, 0], [0, 1, 0], [0, 0, 1]], 'WIN'],
+        ])(
+            'change gameState to win if row/col/dia is completed - (#%#)',
+            (input, expected) => {
+                const board = { ...dummyState, board: input, turnNumber: 5 };
+                expect(
+                    ticTacToeReducer(board, { type: null }).gameState
+                ).toEqual('ACTIVE');
+                const result = ticTacToeReducer(board, action);
+                expect(result.gameState).toEqual(expected);
+            }
+        );
     });
 });
--- src/reducers/tic-tac-toe-reducer.js
+++ src/reducers/tic-tac-toe-reducer.js
@@ -1,11 +1,33 @@
 export function ticTacToeReducer(state = {}, action) {
+    // const newState = Object.assign({}, state);
     const newState = { ...state };
     switch (action.type) {
         case 'MAKE_MOVE':
             const { location } = action;
+            newState.board = newState.board.map(x => x.map(y => y));
             newState.board[location[0]][location[1]] = newState.activePlayer;
-            newState.activePlayer *= -1;
-            newState.turnNumber++;
+            const { board } = newState;
+            if (newState.turnNumber > 4) {
+                const check = [];
+                check.push(
+                    Math.abs(board[location[0]].reduce((acc, val) => acc + val))
+                );
+                check.push(
+                    Math.abs(
+                        board
+                            .map(x => x[location[1]])
+                            .reduce((acc, val) => acc + val)
+                    )
+                );
+                check.push(Math.abs(board[0][0] + board[1][1] + board[2][2]));
+                check.push(Math.abs(board[2][0] + board[1][1] + board[0][2]));
+                if (newState.turnNumber === 9) newState.gameState = 'TIE';
+                if (check.includes(3)) newState.gameState = 'WIN';
+            }
+            if (newState.gameState === 'ACTIVE') {
+                newState.activePlayer *= -1;
+                newState.turnNumber++;
+            }
             break;
         default:
             break;
